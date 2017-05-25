0099745d0efa1bbeb6334fa186a368a7067a4c18
Rafael Neves <rafaeljneves@gmail.com>
2017-05-25 16:19:35 -0300

    criação da função extract_names, refatorando main()

--- google-python-exercises/babynames/babynames.py
+++ google-python-exercises/babynames/babynames.py
@@ -8,6 +8,7 @@
 
 import sys
 import re
+import os
 
 """Baby Names exercise
 
@@ -41,8 +42,37 @@ def extract_names(filename):
     followed by the name-rank strings in alphabetical order.
     ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
     """
-    # +++your code here+++
-    return
+    # -Extract the year and print it
+    with open(filename, 'r') as arquivo:
+        dict_rank_names = {}
+        list_rank_names = []
+        for linha in arquivo:
+            ano = re.search('(Popularity in )([1-2][0-9]{3})', linha)
+            rank = re.search('<td.*?>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td>', linha)
+
+            if ano:
+                list_rank_names.insert(0, ano.group(2))
+                #print(ano.group(2))
+
+            # -Extract the names and rank numbers and just print them
+            if rank:
+                #print(rank.group(2), rank.group(3), rank.group(1))
+
+                # -Get the names data into a dict and print it
+                for i in range(2, 4):
+                    key, values = rank.group(i), rank.group(1)
+                    dict_rank_names[key] = values
+                #print(dict_rank_names)
+
+        # -Build the [year, 'name rank', ... ] list and print it
+        for k, v in sorted(dict_rank_names.items(), reverse=True):
+            list_rank_names.insert(0, k + ' ' + v)
+
+        list_rank_names[0] = list_rank_names[-1]
+        list_rank_names.pop(-1)
+
+
+    return list_rank_names
 
 
 def main():
@@ -61,39 +91,13 @@ def main():
         summary = True
         del args[0]
 
-        # +++your code here+++
-        # For each filename, get the names, then either print the text output
-        # or write it to a summary file
+    # For each filename, get the names, then either print the text output
+    # or write it to a summary file
+    for root, dirs, files in os.walk(os.getcwd()):
+        for file in sorted(files):
+            if file.endswith('.html'):
+                print(extract_names(file))
 
-    #-Extract the year and print it
-    with open('baby1990.html', 'r') as arquivo:
-        dict_rank_names = {}
-        list_rank_names = []
-        for linha in arquivo:
-            ano = re.search('(^<h3.*)([1-2][0-9]{3})', linha)
-            rank = re.search('<td.*?>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td>', linha)
-
-            if ano:
-                list_rank_names.insert(0, ano.group(2))
-                print(ano.group(2))
-
-            # -Extract the names and rank numbers and just print them
-            if rank:
-                print(rank.group(2), rank.group(3), rank.group(1))
-
-                # -Get the names data into a dict and print it
-                for i in range(2,4):
-                    key, values = rank.group(i), rank.group(1)
-                    dict_rank_names[key] = values
-                print(dict_rank_names)
-
-        #-Build the [year, 'name rank', ... ] list and print it
-        for k,v in sorted(dict_rank_names.items(), reverse=True):
-            list_rank_names.insert(0, k + ' ' + v )
-
-        list_rank_names[0] = list_rank_names[-1]
-        list_rank_names.pop(-1)
-        print(list_rank_names)
 
 if __name__ == '__main__':
     main()
